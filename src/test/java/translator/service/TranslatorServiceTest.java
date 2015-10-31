08484c3572482b8cf8252c2671ddf3a3fd2b9ab8
Oscar Clemente Pedrico <oscar.clemente@gmail.com>
2015-10-31 17:04:01 +0100

    encrypted

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test not a show about philosophy.");
+        assertEquals("Esta es una prueba no un espectáculo acerca de la filosofía.",translatedText.getTranslation());
     }
 
 }
